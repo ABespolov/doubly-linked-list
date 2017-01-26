f893b2c6f40510410af6a5f8fa91d01d057a6b86
Артем Бесполов < >
2017-01-26 14:38:46 +0400

    test

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,12 +1,27 @@
 
 const Node = require('./node');
 
-class LinkedList extends Node{
+class LinkedList {
 
+    constructor() {
+        this.length = 0;
+        this.head = null;
+    }
 
     append(data) {
+        var node = new Node();
 
-
+        if (this.length == 0) {
+            this.head = node;
+            node.data = data;
+        }
+        else {
+            this.head.next = node;
+            node.prev = this.head;
+            this.head = node;
+            node.data = data;
+        }
+        this.length++;
     }
 
     head() {}
